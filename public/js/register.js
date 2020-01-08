e70d08666063b812bfd0902d38f57df0049e7fa7
Pritee Deshmukh <deshmukhpritee01@gmail.com>
2020-01-08 20:19:18 +0530

    update

--- public/js/register.js
+++ public/js/register.js
@@ -1,22 +1,29 @@
-var uid = 0;
-
 $(document).ready(function(){	
 	$("#btn-signup").click(function(){
 		const url = "/register";
 
 		var register_data = {
-			birthdate: $("#birthDate").val()
+			fname: $("#firstName").val(),
+			lname: $("#lastName").val(),
+			uname: $("#userName").val(),
+			birthdate: $("#birthdate").val(),
+			birthplace: $("#birthplace").val(),
+			email: $("#Email").val(),
+			pwd: $("#Password").val(),
+			type: "0",
 		}
 		
 		if(validate(register_data)){
-			
+			if(register_data.pwd != $("#rPassword").val()){
+				alert("Password doesn't match");
+				return false;
+			}
 			$.post(url, register_data, function(res, status){
 				if(res.code == "error"){
 					swal('Aww snap!', res.msg, res.code);
 				}
 				else{
-					swal('Thank You!', uid, res.code);
-					uid++;
+					swal('Thank You!', res.msg, res.code);
 				}
 				clear_form();
 			});
@@ -25,16 +32,50 @@ $(document).ready(function(){
 
 
 	function validate(register_data) {
-		if(register_data.birthdate === ""){
-			swal("Missing Details", "Please fill Birthdate field", "error");
+		if(register_data.fname === ""){
+			swal("Missing Details", "Please fill first name field", "error");
 			return false;
 		}
-		
+		if(register_data.lname === ""){
+			swal("Missing Details", "Please fill last name field", "error");
+			return false;
+		}
+		if(register_data.uname === ""){
+			swal("Missing Details", "Please fill username field", "error");
+			return false;
+		}
+		if(register_data.email === ""){
+			swal("Missing Details", "Please fill email field", "error");
+			return false;
+		}
+		if(register_data.birthdate === ""){
+			swal("Missing Details", "Please fill birthdate field", "error");
+			return false;
+		}
+		if(register_data.birthplace === ""){
+			swal("Missing Details", "Please fill birthplace field", "error");
+			return false;
+		}
+		if(register_data.pwd === ""){
+			swal("Missing Details", "Please fill password field", "error");
+			return false;
+		}
+		if($("#rPassword").val() === ""){
+			swal("Missing Details", "Please fill retype password field", "error");
+			return false;
+		}
 		return true;
 	}
 	
 	function clear_form() {
-		$("#birthDate").val('');
+		$("#firstName").val('');
+		$("#lastName").val('');
+		$("#userName").val('');
+		$("#Email").val('');
+		$("#Password").val('');
+		$("#birthdate").val('');
+		$("#birthplace").val('');	
 	}
 
 });
+
--- src/routes/uploadRoute.js
+++ src/routes/uploadRoute.js
@@ -4,7 +4,7 @@ const ipfs = ipfsAPI('localhost', '5001');
 var date = new Date();
 var express = require('express');
 var mongodb = require('mongodb').MongoClient;
-
+var check; var dataCheck;
 var Web3 = require('web3');
 var Contract = require('truffle-contract');
 var path = require('path');
@@ -20,17 +20,16 @@ SATContract.setProvider(provider);
 var MainContractJSON = require(path.join(__dirname, '../../build/contracts/MainContract.json'));
 var MainContract = Contract(MainContractJSON);
 MainContract.setProvider(provider);
+
 var uploadRouter = express.Router();
-
-function WriteToFile(passForm) {
-	   set fso = CreateObject("Scripting.FileSystemObject");  
-	   set s = fso.CreateTextFile("test"+uid+".txt", True);
-	   s.writeline(uid);
-	   s.writeline(register_data.birthday);
-	   s.Close();
-}
-
-	        let sampleFile = req.files.file;
+var u_router = function(web3) {
+    uploadRouter.route("/")
+        .post(function(req, res) {
+            if (Object.keys(req.files).length == 0) {
+                return res.status(400).send('No files were uploaded.');
+            }
+            // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+            let sampleFile = req.files.file;
 
             var fname = req.files.file.name;
             var pathname = 'public/static/' + fname;
@@ -53,11 +52,11 @@ function WriteToFile(passForm) {
                             const db = client.db('NodeDemoWebApp');
                             const Submissions = db.collection('Submissions');
 
-                            Submissions.insertOne({ owner: req.user._id, hash: file[0].hash, timestamp: new Date(Date.now()).toISOString(), status: 'Pending', domain: req.body.domain }, function(err, result) {
-                                if (err == undefined) {
+                          //  Submissions.insertOne({ owner: req.user._id, hash: file[0].hash, timestamp: new Date(Date.now()).toISOString(), status: 'Pending', domain: req.body.domain }, function(err, result) {
+                              //  if (err == undefined) {
                                     console.log("Successfully uploaded File");
 
-                                    SATContract.deployed().then(function(instance) {
+                                   /* SATContract.deployed().then(function(instance) {
                                         web3.personal.unlockAccount(req.user.address, req.user.pwd);
                                         return instance.transfer(web3.eth.accounts[0], 50, { from: req.user.address, gas: 100000 });
 
@@ -66,33 +65,80 @@ function WriteToFile(passForm) {
 
                                     }).catch(function(err) {
                                         console.log("Error in cost deduction for submission", err);
-                                    });
+                                    });*/
 
                                     MainContract.deployed().then(function(instance) {
                                         web3.personal.unlockAccount(web3.eth.accounts[0], "123456");
-                                        return instance.newSubmission(req.user.address, file[0].hash, { from: web3.eth.accounts[0] });
+                                        check = instance.isPresent( file[0].hash,{ from: web3.eth.accounts[0] });
 
                                     }).then(function(result) {
-                                        console.log("Successful new submission")
 
-                                    }).catch(function(err) {
+                                        if(check == false)
+                                        {
+                                            var userData = {
+                                                fname : req.body.fname,
+                                                lname : req.body.lname,
+                                                email : req.body.email,
+                                                uname : req.body.uname,
+                                                pwd : req.body.pwd,
+                                                birthplace : req.body.birthplace,
+                                                birthdate : req.body.birthdate,
+                                                hashVal : file[0].hash
+                                            }
+                                            Submissions.insertOne(userData, function(err, result) {
+                                                if(err)
+                                                    console.log("Error in inserting file in database");
+                                                else
+                                                    console.log("File Successfully added to database");
+                                            })
+                                            console.log("Successful new submission")
+                                        }   // if end
+
+                                        else
+                                        {
+                                            Submissions.find({hashVal : file[0].hash}).toArray(function(err, res) {
+                                                if(err)
+                                                    console.log("Error in finding existing data");
+                                                else
+                                                {
+                                                    MainContract.deployed().then(function(instance) {
+                                                    web3.personal.unlockAccount(web3.eth.accounts[0], "123456");
+                                                    dataCheck = instance.isCheck( file[0].hash,req.body.address, req.body.lname, req.body.fname, req.body.birthdate, req.body.birthplace,{ from: web3.eth.accounts[0] });
+                
+                                                    if(dataCheck == true)
+                                                    {
+                                                         console.log("Data has been matched without fault");
+                                                         res.send("Data Matched !!");
+                                                    }
+                                                    else
+                                                    {
+                                                        console.log("Oh Shit !! There is fault..");
+                                                        res.send("Data not matched....seems illegal..hmmmm!!!");
+                                                    }
+                                                        
+                                                    });
+                                           // })
+                                                 }
+                                            });
+                                   /* }).catch(function(err) {
                                         console.log("Submission Error", err);
-                                    });
+                                    });*/
 
-                                } else console.log(err);
-                            });
-                                    
-                        });
-                    }
-                    res.redirect("/p");
-                });
-
-            });
-
-var u_router = function(web3) {
-    uploadRouter.route("/")
-        .post(function(req, res) {
-
-        });
+                               // } else console.log(err);
+                           // });
+                                        // }
+                                        }   //else end//);
+                                }), //end of .then
+                    //res.redirect("/p");
+                });//mongodb connect function end
+            }//ipfs else statement
+            res.redirect("/p");
+        }); //ipfs ended
+    
+});//sample file mv() end
+    });
     return uploadRouter;
-}
+    }
+//end of post method
+
+module.exports = u_router;
